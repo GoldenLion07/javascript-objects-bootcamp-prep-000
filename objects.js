017a9fd19fbd5968837854cef96cab4b86796fe8
gmat191@gmail.com <gmat191@gmail.com>
2018-06-09 23:45:25 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  artist1: 'Slowdive',
+  artist2: 'My Bloody Valentine',
+  artist3: 'Phil Ochs'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: 'Phil Ochs'})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
