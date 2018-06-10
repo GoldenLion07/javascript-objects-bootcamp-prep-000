48528013e34656cae4d3058f22a2c8593a83770b
gmat191@gmail.com <gmat191@gmail.com>
2018-06-10 00:07:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: "Here's to the State of Mississippi"})
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName) {
