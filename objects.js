b1f91ab6325e7bba752ed584df56e0393350d0e1
gmat191@gmail.com <gmat191@gmail.com>
2018-06-10 00:30:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({})
+  return Object.assign({} playlist)
 }
 
 function removeFromPlaylist(playlist, artistName) {
