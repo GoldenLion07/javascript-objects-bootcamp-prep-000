41b85e8f20acf784099d5870d57432957343645a
gmat191@gmail.com <gmat191@gmail.com>
2018-06-10 00:06:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  return Object.assign({}, playlist, {[Phil Ochs]: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName) {
