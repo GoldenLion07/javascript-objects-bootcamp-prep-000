dec29a7e5d4353c32426429cffc255ad5665c23c
gmat191@gmail.com <gmat191@gmail.com>
2018-06-10 00:15:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,8 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return playlist.add()
+  return playlist[artistName];
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
