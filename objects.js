94f5e53f1b8373c3b1420cfeffa11bf50970d058
gmat191@gmail.com <gmat191@gmail.com>
2018-06-10 00:04:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,18 +4,6 @@ var playlist = {
   PhilOchs: "Here's to the State of Mississippi"
 }
 
-var artistName = {
-  artistName1: "Slowdive",
-  artistName2: "My Bloody Valentine",
-  artistName3: "Phil Ochs"
-}
-
-var songTitle ={
-  song1:"Alison",
-  song2:"Sometimes",
-  song3:"Here's to the State of Mississippi"
-}
-
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
